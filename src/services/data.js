a57e703e1032921285f2e98dff2fe3809b2db45b
Crab-Prog <ophelie.coudert@gmail.com>
2022-07-22 15:19:57 +0200

    added the last project

--- src/services/data.js
+++ src/services/data.js
@@ -3,8 +3,15 @@ import projet1 from "../assets/images/projet1.jpg";
 import projet2 from "../assets/images/projet2.jpg";
 import frontEndChallenge from "../assets/images/front-end-challenge.jpg";
 import codeWars from "../assets/images/codewars.jpg";
+import checkpoint from "../assets/images/checkpoint4.jpg";
 
-const projectDone = [
+const projectDone = [  
+  {
+    img: checkpoint,
+    title: "INS school refonte",
+    desc: "Disponible sur Github pour le moment",
+    lien: "https://github.com/Crab-Prog/2022-03-JS-RemoteFR-Outsiders-checkpoint-4",
+  },
   {
     img: hackathon,
     title: "Hackathon 1 - Team Undefined",
@@ -35,6 +42,7 @@ const projectDone = [
     desc: "Profil code wars",
     lien: "https://www.codewars.com/users/Crab-Prog",
   },
+
 ];
 
 export default projectDone;
